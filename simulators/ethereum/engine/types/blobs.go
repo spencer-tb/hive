fdda63a5a5e7112bd7b8c29d534bcdd1c1518940
Mario Vega <marioevz@gmail.com>
2023-06-27 23:51:25 +0000

    simulators/ethereum/engine: BlobBundle has VersionedHashes

--- simulators/ethereum/engine/types/blobs.go
+++ simulators/ethereum/engine/types/blobs.go
@@ -146,3 +146,18 @@ type BlobsBundle struct {
 	Blobs       []Blob          `json:"blobs"       gencodec:"required"`
 	Proofs      []KZGProof      `json:"proofs"      gencodec:"required"`
 }
+
+func (bb *BlobsBundle) VersionedHashes(commitmentVersion byte) ([]common.Hash, error) {
+	if bb == nil {
+		return nil, errors.New("nil blob bundle")
+	}
+	if bb.Commitments == nil {
+		return nil, errors.New("nil commitments")
+	}
+	versionedHashes := make([]common.Hash, len(bb.Commitments))
+	for i, commitment := range bb.Commitments {
+		sha256Hash := sha256.Sum256(commitment[:])
+		versionedHashes[i] = common.BytesToHash(append([]byte{commitmentVersion}, sha256Hash[1:]...))
+	}
+	return versionedHashes, nil
+}
